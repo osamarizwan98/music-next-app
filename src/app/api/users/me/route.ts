f8c35b2c86c69a3ce3ebdaaac5bb3308d548620c
osamarizwan98 <osamarizwan98@gmail.com>
2024-06-07 16:05:41 +0500

    update profile route

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -8,7 +8,7 @@ connect();
 export async function GET(request: NextRequest) {
   try {
     const userId = await getDataFromToken(request);
-    const user = User.findOne({ _id: userId }).select("-password");
+    const user = await User.findOne({ _id: userId }).select("-password");
     return NextResponse.json({
       message: "User found",
       data: user,
